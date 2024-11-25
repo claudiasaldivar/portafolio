29ed6f14594138423bc2ce8ba2acd60306bab39a
claudiasaldivar <claudiasaldivar493@gmail.com>
2024-11-25 08:41:45 -0600

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: '/portafolio',
+  assetPrefix: '/portafolio/',
   output: 'export',
   images: {
     unoptimized: true,
